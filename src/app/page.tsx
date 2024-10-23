64ce03471b366e23d73f3e2fbb9ad7b828a37cbe
YenTingWu <a9600125a@gmail.com>
2024-10-23 14:36:10 +0800

    feat: webcam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
+import ReactWebcam from "react-webcam";
 import liff from "@line/liff";
 import Image from "next/image";
 
 type Profile = Awaited<ReturnType<typeof liff.getProfile>>;
 
 export default function Home() {
+  const [isInit, setIsInit] = useState<boolean>(false);
   const [lineProfile, setLineProfile] = useState<Profile | null>(null);
   const [errorMessage, setErrorMessage] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+  const webcamRef = useRef<ReactWebcam>(null);
+
+  const capture = () => {
+    if (webcamRef.current === null) return;
+    const imageSrc = webcamRef.current.getScreenshot();
+
+    setImage(imageSrc);
+  };
 
   const getProfile = async () => {
     try {
@@ -26,6 +37,7 @@ export default function Home() {
           liffId: process.env["NEXT_PUBLIC_LINE_LIFF_ID"] as string,
         });
         console.log("liff.init() success");
+        setIsInit(true);
       } catch (error) {
         console.error("liff.init() failed", error);
       }
@@ -33,6 +45,10 @@ export default function Home() {
 
     startLiff();
   });
+
+  if (isInit === false) return null;
+
+  const isLogin = liff.isLoggedIn();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -46,20 +62,23 @@ export default function Home() {
           priority
         />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={async () => {
-              await liff.login();
-            }}
-          >
-            LOGIN
-          </button>
-          <button
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            onClick={getProfile}
-          >
-            SHOW PROFILE
-          </button>
+          {isLogin ? (
+            <button
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              onClick={getProfile}
+            >
+              SHOW PROFILE
+            </button>
+          ) : (
+            <button
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              onClick={async () => {
+                await liff.login();
+              }}
+            >
+              LOGIN
+            </button>
+          )}
         </div>
         <div className="break-all">
           <ul>
@@ -71,54 +90,32 @@ export default function Home() {
           </ul>
         </div>
         {errorMessage && <small className="text-red-400">{errorMessage}</small>}
+        {isLogin && (
+          <div className="rounded-xl ring-2 shadow-xl flex flex-col items-center p-4">
+            <ReactWebcam ref={webcamRef} mirrored />
+            <div className="flex items-center gap-4">
+              <button
+                className="rounded ring-1 shadow py-2 px-4 mt-3"
+                onClick={capture}
+              >
+                Capture
+              </button>
+              <button
+                className="rounded bg-sky-100 shadow py-2 px-4 mt-3"
+                onClick={() => setImage(null)}
+              >
+                Clear
+              </button>
+            </div>
+            {image && (
+              <div className="mt-10">
+                <h3>Your cool image</h3>
+                <Image src={image} alt="image" width={400} height={400} />
+              </div>
+            )}
+          </div>
+        )}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
